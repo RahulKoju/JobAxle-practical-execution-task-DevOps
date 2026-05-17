06290b951fb963bf1c97a6939df4c661afcf06bd
Rahul Koju <blcraz75@gmail.com>
2026-05-17 14:33:31 +0545

    feat: update home message in branch B

--- src/index.js
+++ src/index.js
@@ -2,9 +2,7 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) =>
-  res.json({ message: "Hello from DevOps Node App!" }),
-);
+app.get("/", (req, res) => res.json({ message: "Hello from Branch B!" }));
 app.get("/health", (req, res) => res.json({ status: "ok" }));
 app.get("/users", (req, res) => res.json({ users: ["Ram", "Shyam"] }));
 
